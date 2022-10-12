8ce68123719f34eb75e100f52d20e9956b7ca70a
MayaL <lemayalerman@gmail.com>
2022-10-12 14:02:32 +0200

    text and pulp.ai

--- assets/scripts/arrayFemmes.js
+++ assets/scripts/arrayFemmes.js
@@ -5,8 +5,10 @@ let femmes = [
     nom: "Greta Thunberg",
     phrase: "Une militante écologiste suédoise",
     dateNaissance: "01/03/2003",
-    hint1: "hint1",
-    hint2: "hint2",
+    hint1:
+      "La grève de Greta a attiré l'attention des médias du monde entier, et a incité des dizaines de milliers d'écoliers autour du monde à faire de même",
+    hint2:
+      "Selon Greta, la multiplication des grèves de l'école pour le climat est une bonne chose, 'Cela prouve que nous ne sommes jamais trop petits pour faire la différence'",
     bibli:
       "Greta Thunberg, née le 3 janvier 2003 à Stockholm (19 ans), est une jeune militante écologiste suédoise engagée dans la lutte contre le réchauffement climatique.Elle est connue parce qu'elle fait chaque vendredi la grève de l'école devant le parlement suédois, mais aussi parce qu'elle a fait des conférences importantes : elle a par exemple pris la parole devant les dirigeants du monde entier lors de la COP24, une conférence organisée par l'ONU en Pologne en décembre 2018, et est intervenue en France à l'Assemblée nationale.",
   },
@@ -20,9 +22,9 @@ let femmes = [
     hint1:
       "le livre Le Journal d'Anne Frank écrit pendant les deux années où elle vivait cachée avec sa famille à Amsterdam aux Pays-Bas, sous occupation allemande",
     hint2:
-      "le texte étant traduit dans plus de 70 langues5 et serait le livre le plus traduit et le plus lu dans le monde après la Bible",
+      "le texte étant traduit dans plus de 70 langues et serait le livre le plus traduit et le plus lu dans le monde après la Bible",
     bibli:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
+      "Anne Frank née le 12 juin 1929 à Francfort, décédée en février ou mars 1945 du typhus à Bergen-Belsen est une jeune fille juive qui est connue pour avoir survécu en Allemagne nazie durant presque toute la Seconde Guerre mondiale, mais est morte (ainsi que presque toute sa famille sauf Otto, son père) peu avant la fin de la guerre.Anne Frank est célèbre pour avoir écrit Le Journal d'Anne Frank, un journal intime qui lui est offert pour son treizième anniversaire. Elle commence à écrire dedans deux jours après l'avoir reçu. Elle raconte, entre autres, comment sa famille et quatre amis ont vécu de 1942 à 1944, cachés dans une annexe de bureaux, grâce aux employés de l'entreprise que son père. Le journal d'Anne Frank (rédigé en néerlandais) a été publié par son père peu après la libération, pour lui rendre hommage, sous forme d'un livre qui est devenu le plus traduit au monde après la Bible1. Ce document est non seulement très précieux du point de vue historique, mais aussi très apprécié pour le talent littéraire d'Anne Frank.",
   },
   {
     id: "3",
@@ -30,10 +32,12 @@ let femmes = [
     nom: "Rosa Parks",
     phrase: "La mère du mouvement des droits civiques",
     dateNaissance: "02/04/1913",
-    hint1: "hint1",
-    hint2: "hint2",
+    hint1:
+      "Elle a refusé de laisser sa place à quelqu'un à la peau blanche dans le bus. En effet, à cette époque, les personnes de couleur noire devaient laisser leur place aux gens à la peau blanche dans le bus.",
+    hint2:
+      " La veille de son jugement, un jeune pasteur noir, Martin Luther King, a lancé un boycott contre la compagnie de bus.",
     bibli:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
+      "Rosa Parks est née le 4 février 1913 en Alabama et morte le 24 octobre 2005 à 92 ans à Michigan. Elle est devenue célèbre en 1955, car elle est à l'origine de la lutte pour l'égalité entre les personnes blanches et les gens de couleur aux États-Unis. Le 1er décembre 1955, alors qu'elle a 42 ans, après une journée de travail, Rosa prend le bus pour rentrer chez elle. Elle s'assoit au centre du bus, sur une place réservée aux personnes blanches alors que Rosa est noire. À côté d'elle il y a trois voyageurs également noirs. Un blanc monte dans le bus et le chauffeur demande à Rosa et à ses voisins de laisser leur place pour les céder au nouveau voyageur. Selon les lois en vigueur les noirs ne peuvent voyager que dans le fond du bus, dans la zone qui leur est réservée. Rosa Parks refuse de céder sa place. Le chauffeur appelle la police locale. Rosa Parks est arrêtée.  Pendant 380 jours, des milliers de personnes refusent d'entrer dans un bus, et vont à leur travail à pied, en partageant leurs voitures ou en taxi. Le 13 novembre 1956, la cour suprême vote la fin de la ségrégation raciale dans les bus municipaux. Elle représente le combat pour la défense des droits civiques des personnes de couleur noires. Elle a fait avancer le monde en luttant contre la ségrégation raciale. En 2013, le président Barack Obama a inauguré sa statue dans la galerie du Capitole où sont représentés les personnages majeurs de l'histoire des États-Unis. On parle de Rosa Parks pour figurer sur les prochains billets de 20 dollars.",
   },
   {
     id: "4",
@@ -41,10 +45,12 @@ let femmes = [
     nom: "Hatchepsout",
     phrase: "une reine de l'Égypte antique qui deviendra pharaon",
     dateNaissance: "1/1/1000",
-    hint1: "hint1",
-    hint2: "hint2",
+    hint1:
+      " Sa momie a été retrouvée dans la Vallée des Rois par Howard Carter en 1903",
+    hint2:
+      "Hatchepsout est la fille du pharaon Thoutmosis Ier et de la reine Ahmès",
     bibli:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
+      "Hatchepsout est une femme-pharaon cinquième souverain de la XVIIIe dynastie. Elle est principalement connue pour être une des seules femmes à avoir occupé cette fonction. Elle a régné aux alentours de -1479 à -1457 ou, selon d'autres historiens, de - 1498 à -14831 . Hatchepsout est la fille du pharaon Thoutmosis Ier et de la reine Ahmès. Elle épouse son demi-frère, le pharaon Thoutmosis II. Elle monte sur le trône à la mort de son mari, En fait, elle règne avec le fils de ce dernier, Thoutmôsis III, alors trop jeune pour diriger seul le royaume.Elle a notamment construit son temple avec de magnifiques statues et colonnades sur les terrasses. 120 sphinx gardent l'entrée du temple. Sa momie a été retrouvée dans la Vallée des Rois par Howard Carter en 1903, puis a été identifiée en 2007. Elle repose au Musée du Caire. ",
   },
   {
     id: "5",
@@ -52,8 +58,11 @@ let femmes = [
     nom: "Grace O'Malley",
     phrase: "Une pirate irlandaise",
     dateNaissance: "01/01/1530",
+    hint1:
+      "Grace O'Malley est un personnage du folklore irlandais qui a réellement existé, une femme pirate que les enfants adorent !",
+    hint2: "",
     bibli:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
+      "Reine du royaume d’Umaill en Irlande, Grace O’Malley (vers 1530 – vers 1603) est une femme pirate et une figure historique irlandaise. Dès son plus jeune âge, et à l'encontre des coutumes de l'époque, elle passe son temps sur les bateaux avec son père. En 1546, Grace O’Malley est mariée à Donal O’Flaherty, héritier et futur roi de Connacht. Ils auront trois enfants : Owen, Margaret et Murrough. Avec son mari, Grace se lance dans le commerce et la navigation. Des plaintes rapportent alors que leurs bateaux se comportent comme des pirates, imposant par la force des taxes de passage. Finalement, Donal est tué à la bataille et Grace gagne le respect de ses hommes et de ses adversaires en défendant le château de Donal contre des envahisseurs irlandais puis anglais, malgré une infériorité numérique. ",
   },
 
   {
@@ -73,8 +82,7 @@ let femmes = [
     id: "7",
     img: "katherinejohnson.png",
     nom: "Katherine Johnson",
-    phrase:
-      " Mathématicienne afro-américaine et employée de la NASA",
+    phrase: " Mathématicienne afro-américaine et employée de la NASA",
     dateNaissance: "08/26/1918 ",
     hint1: "hint1",
     hint2: "hint2",
@@ -111,8 +119,7 @@ let femmes = [
     id: "10",
     img: "baya.png",
     nom: "Baya Mahieddine",
-    phrase:
-      "Peintre autodidacte algérienne",
+    phrase: "Peintre autodidacte algérienne",
     dateNaissance: "12/12/1931 ",
     hint1: "Son oeuvre est qualifiée de surréaliste et naïve",
     hint2: "Elle a inspiré Picasso et Matisse",
@@ -157,22 +164,10 @@ let femmes = [
     bibli:
       "Elle est née en 1907 à Coyoacán au Mexique. À l'âge de 6 ans, elle est malade de la polio ce qui la laisse handicapée; certains spécialistes pensent qu'elle devait souffrir d'une malformation de naissance, empêchant le bon développement de sa colonne vertébrale.Elle commence à étudier la médecine pour devenir médecin mais, à l'âge de 18 ans, elle est victime d'un grave accident de la circulation dans lequel son bus percute un métro. Pendant toute sa vie, elle garde de cet accident des périodes de grande douleur physique. Elle abandonne alors ses études de médecine et commence à peindre, s'inspirant des évènements survenus dans sa vie.Sa famille aménage un lit à baldaquin avec un miroir accroché à l'horizontale car elle est immobilisée dans son lit suite à son accident. Grâce à ceci Frida Kahlo a pu peindre 55 autoportraits sur 143 tableaux.Ses tableaux montrent sa douleur et la vie difficile des femmes, en particulier ses regrets de ne pas pouvoir être mère.Elle épouse Diego Rivera en 1929, mais ils ont chacun de leur côté des aventures amoureuses. Tous deux sont membres du Parti communiste mexicain. En 1937, ils accueillent pendant quelques semaines dans leur demeure le révolutionnaire russe Léon Trotsky et son épouse Natalia : Trotsky a été expulsé en 1929 de l'Union Soviétique par Staline, et le Mexique vient de lui accorder l'asile politique. Rivera et Frida Kahlo divorcent en 1939 (Rivera aurait eu une liaison avec Cristina Kahlo, la propre sœur de Frida), mais ils se remarient ensemble en 1940 et leur union dure jusqu'à la mort de Frida Kahlo, en 1954, des suites d'une embolie pulmonaire à l'âge de 47 ans.",
   },
-<<<<<<< HEAD
-=======
-
->>>>>>> 4420ee01e547cbead51c47a560965673ff9e74e6
   {
     id: "14",
     img: "adalovelace.png",
     nom: "Ada Lovelace",
-<<<<<<< HEAD
-    phrase:
-      "Peintre autodidacte algérienne, surréaliste et naïve, a inspiré Picasso et Matisse",
-    dateNaissance: "12/10/1815 ",
-    hint1: "hint1",
-    hint2: "hint2",
-  },
-=======
     phrase: "Considérée comme la première programmeuse en informatique",
     dateNaissance: "12/10/1815 ",
     hint1: "hint1",
@@ -181,65 +176,39 @@ let femmes = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
   },
 
->>>>>>> 4420ee01e547cbead51c47a560965673ff9e74e6
   {
     id: "15",
     img: "janegoodall.png",
     nom: "Jane Goodall",
-<<<<<<< HEAD
-    phrase:
-      "Peintre autodidacte algérienne, surréaliste et naïve, a inspiré Picasso et Matisse",
-    dateNaissance: "04/03/1934 ",
-    hint1: "hint1",
-    hint2: "hint2",
-=======
     phrase: "Primatologue et anthropologue anglaise",
     dateNaissance: "04/03/1934 ",
     hint1: "hint1",
     hint2: "hint2",
     bibli:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
->>>>>>> 4420ee01e547cbead51c47a560965673ff9e74e6
   },
 
   {
     id: "16",
     img: "mariamontessori.png",
     nom: "Maria Montessori",
-<<<<<<< HEAD
-    phrase:
-      "Peintre autodidacte algérienne, surréaliste et naïve, a inspiré Picasso et Matisse",
-    dateNaissance: "10/31/1870 ",
-    hint1: "hint1",
-    hint2: "hint2",
-=======
     phrase: "Une éducatrice italienne célèbre pour sa méthode",
     dateNaissance: "08/31/1870 ",
     hint1: "hint1",
     hint2: "hint2",
     bibli:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
->>>>>>> 4420ee01e547cbead51c47a560965673ff9e74e6
   },
 
   {
     id: "17",
     img: "mariecurie.png",
     nom: "Marie Curie",
-<<<<<<< HEAD
-    phrase:
-      "Peintre autodidacte algérienne, surréaliste et naïve, a inspiré Picasso et Matisse",
-    dateNaissance: "12/07/1867 ",
-    hint1: "hint1",
-    hint2: "hint2",
-=======
     phrase: "Une célèbre chimiste franco-polonaise",
     dateNaissance: "11/07/1867 ",
     hint1: "hint1",
     hint2: "hint2",
     bibli:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis sunt repudiandae maxime consectetur, suscipit architecto, optio, harum doloremque obcaecati magnam dolorum aperiam similique quis velit. Possimus ea beatae aliquam.Laudantium aliquid illo, fuga quidem commodi fugiat earum hic eligendi possimus dolorum soluta non corrupti tenetur sequi obcaecati dolor nam velit provident odio quis quisquam blanditiis mollitia laboriosam! Ullam, quas.",
->>>>>>> 4420ee01e547cbead51c47a560965673ff9e74e6
   },
 ];
-
